aeba89620c750b6ea3190d10ff02b8bee85881b1
YonasGetaw <yonasgetaw5444@gmail.com>
2025-03-17 10:22:42 -0700

    update java

---
+++ JAVA/firstjava.java
@@ -0,0 +1,50 @@
+// import java.util.Scanner;
+// class firstjava{
+// public static void main(String args[])
+// {
+// Scanner num1=new Scanner(System.in);
+// System.out.print("Enter the first number:");
+// double input=num1.nextInt();
+// Scanner num2=new Scanner(System.in);
+// System.out.println("Enter the second number:");
+// double input=num2.nextInt();
+// double sum =num1+num2;
+// System.out.println("The sum of the two number is :",sum);
+
+// }
+
+// }
+
+class firstjava{
+public  static void main(String args[])
+{
+    System.out.println (" Hello , jone!");
+
+    float x=(float)20;
+    double y=30;
+    double sum = x+y;
+System.out.print("the sum of two number is: "+sum);
+int a=10;
+int b=20;
+System.out.println(" BEFORE SWAPPING ");
+a=a+b;
+b=a-b;
+a=a-b;
+System.out.println(" AFTER SWAPPING ");
+System.out.println("a="+a);
+System.out.printf("b="+b);
+
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ yona.java
@@ -0,0 +1,9 @@
+public class Area{
+private String Name;
+private String Id;
+private String Age;
+public Area(String Name ,String Id, int Age){
+    
+}
+
+}
